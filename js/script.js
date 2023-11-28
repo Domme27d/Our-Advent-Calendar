78dd62c57f77d070b341da3abea0be650c186782
Domme27d <151401461+Domme27d@users.noreply.github.com>
2023-11-29 00:26:14 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 // script.js
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Your JavaScript code goes here
+document.addEventListener('DOMContentLoaded', function() {
     console.log("Page fully loaded and parsed");
-});
 
+    var toggleButton = document.getElementById('toggleBorderButton');
+    var today = new Date();
+    var currentDay = today.getDate(); // Get the current day as a number
+    var dayLinkId = "day" + (currentDay < 10 ? "0" + currentDay : currentDay); // Adjust for leading zero
+    var dayLink = document.getElementById(dayLinkId);
+
+    toggleButton.addEventListener('click', function() {
+        if (dayLink) {
+            dayLink.classList.toggle('highlight-border');
+        } else {
+            console.log("No link found for the current day: " + dayLinkId);
+        }
+    });
+});
